a2ad409fbf804d161f9009c830b6621d705217cf
Benoît Freslon <contact@benoitfreslon.com>
2019-07-18 13:39:41 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Unity Simle Email Support
+
+Get email feedback and useful informations from player devices just with one button.
+
+* OS
+* Language
+* Version
+* Device informations
+
+## How to use
+
+1. Get **SimpleEmailSupport.cs**
+2. Add this script in a GameObject
+2. Edit the strings: `myEmail` and `subject`
+2. Then call the public method `SendEmail` from UI element
+
---
+++ SimpleEmailSupport.cs
@@ -0,0 +1,39 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//  
+// @author Benoît Freslon @benoitfreslon
+// https://github.com/BenoitFreslon/UnitySimpleEmailSupport
+// https://benoitfreslon.com
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SimpleEmailSupport : MonoBehaviour
+{
+	// Replace to your support email
+	public string myEmail = "support@mystudio.com";
+
+	// No space char in the subject
+	public string subject = "[MyGame] Feedback";
+
+	public void SendEmail()
+	{
+		string body = "\n\n----"
+			+ "\nVersion: " + Application.version
+			+ "\nSystem: " + SystemInfo.operatingSystem
+			+ "\nPlateform: " + SystemInfo.deviceModel
+			+ "\nLanguage: " + Application.systemLanguage;
+
+		body = UnityWebRequest.EscapeURL(body).Replace("+", "%20");
+		subject = subject.Replace(" ", "%20");
+
+		string emailToSend = "mailto:" + myEmail + "?subject=" + subject + "&body=" + body;
+
+		Debug.Log("Send Email: " + emailToSend);
+
+		Application.OpenURL(emailToSend);
+	}
+}
